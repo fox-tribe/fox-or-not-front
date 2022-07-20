8e40305c73f852c83c84b7ad3f79587ff4d5c4fd
attabooi <joonqpa@gmail.com>
2022-07-20 18:51:01 +0900

    마이페이지 메인 및 아이디 연동

--- static/Scripts/boards.js
+++ static/Scripts/boards.js
@@ -1,12 +1,11 @@
 const board_url = window.location.search.split('=')
 const boards_name = board_url[1]
-
-
+const decoded_name = decodeURI(boards_name)
 
 // 게시물 상세 페이지 부르기
 window.onload = async function board() {
     let board = async () => {
-        let response = await fetch(`${backend_base_url}/article/board?boards=${boards_name}`, {
+        let response = await fetch(`${backend_base_url}/article/board?boards=${decoded_name}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -16,21 +15,20 @@ window.onload = async function board() {
     }
     board().then((data) => {
         detail = response_json
-        console.log(detail)
-        decoded_name = decodeURI(boards_name)
-        for (let i = 0; i < detail[0][`${decoded_name}`].length; i++) {
-        let author = detail[0][`${decoded_name}`][i]['author']
-        let title = detail[0][`${decoded_name}`][i]['article_title']
-        let date = detail[0][`${decoded_name}`][i]['article_post_date']
-        const contents = document.createElement("li");
-        contents.classList.add("content");
-        contents.innerHTML = `
-        <span class="content__id">${id}</span>
-        <span class="content__title">${title}</span>
-        <span class="content__author">${author}</span>
-        <span class="content__date">${date}</span>
+        for (let i = 0; i < 3; i++) {
+        const boards_author = detail[0][`${decoded_name}`][i]['author']
+        const boards_title = detail[0][`${decoded_name}`][i]['article_title']
+        const boards_date = detail[0][`${decoded_name}`][i]['article_post_date']
+        const boards_id = detail[0][`${decoded_name}`][i]['id']
+        const content = document.createElement("li");
+        content.classList.add("content");
+        content.innerHTML = `
+        <span class="content__id">${boards_id}</span>
+        <span class="content__title">${boards_title}</span>
+        <span class="content__author">${boards_author}</span>
+        <span class="content__date">${boards_date}</span>
       `;
+        return content;
         }}
     )
 }
-
