dd710bcfcd79c7b18cad386b869cea1340ce467b
Moonmoo <ansaudwn1234@gmail.com>
2022-07-21 17:56:52 +0900

    정보수정 빈칸일시 alert

--- static/Scripts/mypage_user.js
+++ static/Scripts/mypage_user.js
@@ -14,13 +14,9 @@ async function userInfo() {
     }
     userInfo().then((data) => {
         detail = response_json
-        console.log(detail)
         let username = detail['username']
-        console.log(detail['username'])
         let nickname = detail['nickname']
-        console.log(detail['nickname'])
         let gender = detail['gender']
-        console.log(detail['gender'])
         let temp_html =`                <h2>현재정보</h2>
         
         <div class="befbio"><p><span style="color:black;">아이디:</span>   ${username}</p></div>
@@ -37,7 +33,12 @@ function editInfo() {
     let password = document.getElementById("edit-password").value
     let selectGender = document.getElementById("edit-gender").value
 
+    if (nickname =='' || password =='' || selectGender ==''){
+        alert('빈칸을 입력해주세요!')
+    }
+    else {
     changeInfo(nickname, password, selectGender)
+    }
 }
 
 
