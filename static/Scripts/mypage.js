28fd420d153ca97b2290bc2dbd84a73f42fa4e56
seonyoungBaek <rk4418308@gmail.com>
2022-07-22 10:11:02 +0900

    모달오류수정

--- static/Scripts/mypage.js
+++ static/Scripts/mypage.js
@@ -3,7 +3,7 @@ async function modal() {
     const openButton = document.getElementById("modalclick");
     const modal = document.querySelector(".modal");
     const overlay = modal.querySelector(".modal__overlay");
-    const closeBtn = modal.querySelector(".button");
+    const closeBtn = modal.querySelector(".modalbutton");
     const openModal = () => {
         modal.classList.remove("hidden");
     }
