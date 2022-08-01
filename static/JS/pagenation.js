beea5680b97e5ff6a0a6297efc284f6008682010
attabooi <joonqpa@gmail.com>
2022-08-01 17:40:39 +0900

    게시판 페이지네이션 기능 구현

--- static/JS/pagenation.js
+++ static/JS/pagenation.js
@@ -1,89 +1,79 @@
-const contents = document.querySelector(".contents");
-const buttons = document.querySelector(".buttons");
-
-const numOfContent = 178;
-const maxContent = 10;
-const maxButton = 5;
-const maxPage = Math.ceil(numOfContent / maxContent);
-let page = 1;
 
 
-// 게시글 정보 구간
-const makeContent = (id) => {
-    const content = document.createElement("li");
-    content.classList.add("content");
-    content.innerHTML = `
-    <span class="content__id">${id}</span>
-    <span class="content__title">폭스들의 정체를 낱낱히 파헤칩니다</span>
-    <span class="content__author">폭스킹최준헌</span>
-    <span class="content__date">2022.07.15</span>
-  `;
-    return content;
-};
 
-const makeButton = (id) => {
-    const button = document.createElement("button");
-    button.classList.add("button");
-    button.dataset.num = id;
-    button.innerText = id;
-    button.addEventListener("click", (e) => {
-        Array.prototype.forEach.call(buttons.children, (button) => {
-            if (button.dataset.num) button.classList.remove("active");
-        });
-        e.target.classList.add("active");
-        renderContent(parseInt(e.target.dataset.num));
-    });
-    return button;
-};
+// const contents = document.querySelector(".contents");
+// const buttons = document.querySelector(".buttons");
 
-// Prev, Next Button
-const goPrevPage = () => {
-    page -= maxButton;
-    render(page);
-};
+// const numOfContent = 178;
+// const maxContent = 10;
+// const maxButton = 5;
+// const maxPage = Math.ceil(numOfContent / maxContent);
+// let page = 1;
 
-const goNextPage = () => {
-    page += maxButton;
-    render(page);
-};
 
-const prev = document.createElement("button");
-prev.classList.add("button", "prev");
-prev.innerHTML = '<ion-icon name="chevron-back-outline"></ion-icon>';
-prev.addEventListener("click", goPrevPage);
+// const makeButton = (id) => {
+//     const button = document.createElement("button");
+//     button.classList.add("button");
+//     button.dataset.num = id;
+//     button.innerText = id;
+//     button.addEventListener("click", (e) => {
+//         Array.prototype.forEach.call(buttons.children, (button) => {
+//             if (button.dataset.num) button.classList.remove("active");
+//         });
+//         e.target.classList.add("active");
+//         renderContent(parseInt(e.target.dataset.num));
+//     });
+//     return button;
+// };
 
-const next = document.createElement("button");
-next.classList.add("button", "next");
-next.innerHTML = '<ion-icon name="chevron-forward-outline"></ion-icon>';
-next.addEventListener("click", goNextPage);
+// // Prev, Next Button
+// const goPrevPage = () => {
+//     page -= maxButton;
+//     render(page);
+// };
 
-const renderContent = (page) => {
-    while (contents.hasChildNodes()) {
-        contents.removeChild(contents.lastChild);
-    }
-    for (let id = (page - 1) * maxContent + 1; id <= page * maxContent && id <= numOfContent; id++) {
-        contents.appendChild(makeContent(id));
-    }
-};
+// const goNextPage = () => {
+//     page += maxButton;
+//     render(page);
+// };
 
-const renderButton = (page) => {
-    while (buttons.hasChildNodes()) {
-        buttons.removeChild(buttons.lastChild);
-    }
-    for (let id = page; id < page + maxButton && id <= maxPage; id++) {
-        buttons.appendChild(makeButton(id));
-    }
-    buttons.children[0].classList.add("active");
+// const prev = document.createElement("button");
+// prev.classList.add("button", "prev");
+// prev.innerHTML = '<ion-icon name="chevron-back-outline"></ion-icon>';
+// prev.addEventListener("click", goPrevPage);
 
-    buttons.prepend(prev);
-    buttons.append(next);
+// const next = document.createElement("button");
+// next.classList.add("button", "next");
+// next.innerHTML = '<ion-icon name="chevron-forward-outline"></ion-icon>';
+// next.addEventListener("click", goNextPage);
 
-    if (page - maxButton < 1) buttons.removeChild(prev);
-    if (page + maxButton > maxPage) buttons.removeChild(next);
-};
+// const renderContent = (page) => {
+//     while (contents.hasChildNodes()) {
+//         contents.removeChild(contents.lastChild);
+//     }
+//     for (let id = (page - 1) * maxContent + 1; id <= page * maxContent && id <= numOfContent; id++) {
+//         contents.appendChild(makeContent(id));
+//     }
+// };
 
-const render = (page) => {
-    renderContent(page);
-    renderButton(page);
-};
-render(page);
+// const renderButton = (page) => {
+//     while (buttons.hasChildNodes()) {
+//         buttons.removeChild(buttons.lastChild);
+//     }
+//     for (let id = page; id < page + maxButton && id <= maxPage; id++) {
+//         buttons.appendChild(makeButton(id));
+//     }
+//     buttons.children[0].classList.add("active");
+
+//     buttons.prepend(prev);
+//     buttons.append(next);
+
+//     if (page - maxButton < 1) buttons.removeChild(prev);
+//     if (page + maxButton > maxPage) buttons.removeChild(next);
+// };
+
+// const render = (page) => {
+//     renderContent(page);
+//     renderButton(page);
+// };
+// render(page);
