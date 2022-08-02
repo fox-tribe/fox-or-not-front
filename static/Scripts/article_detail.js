3cf2c02e2f53ea02ee0b5310d9eb29f3ea3d8e59
Moonmoo <ansaudwn1234@gmail.com>
2022-08-02 21:58:58 +0900

    공감하트채우기

--- static/Scripts/article_detail.js
+++ static/Scripts/article_detail.js
@@ -87,6 +87,44 @@ window.onload = async function articleDetail() {
             let nickname = detail['comment_set'][i]['nickname']
             let comment_created_at = detail['comment_set'][i]['comment_created_at']
             let comment_like_count = detail['comment_set'][i]['count']
+            if (comment_like_count == 0) {let temp_html =
+                `<div class="comments">
+                    <div class="cowriteinfo">
+                        <div><p class="cowriter">${nickname}</p></div>
+                        <div><p class="cotime">${comment_created_at}</p></div>
+                        <p class="comment-modify" id="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">수정</p>
+                        <p class="comment-delete" onclick="getdeleteComment(${comment_id})"id="#">삭제</p>
+                    </div>
+                    <!-- 댓글 수정 모달 -->
+                    <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">댓글 수정</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    댓글<br>
+                                    <input type="text" class="modal-textinput" placeholder="comment" id="comment-update">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+                                    <button type="button" onclick="getUpdateComment(${comment_id})" class="btn btn-primary">댓글 수정</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="commentdetail">
+                        <div class="comment"><h4>${comments}</h4></div>
+                        <div style="display:flex; flex-direction:row; align-items:center">
+                        <svg xmlns="http://www.w3.org/2000/svg" id="heart-button" class="heart " onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                        <h5 style="margin:0; display:block">${comment_like_count}</h5>
+                        </div>
+                    </div>
+                </div>`
+            $('#comments-box').prepend(temp_html)
+        } else {
             let temp_html =
                 `<div class="comments">
                     <div class="cowriteinfo">
@@ -118,15 +156,14 @@ window.onload = async function articleDetail() {
                     <div class="commentdetail">
                         <div class="comment"><h4>${comments}</h4></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
-                        <button type="button" id="like" class="like" onclick="likeButton(${comment_id})"><i class="fa-regular fa-heart fa-2x"></i></button>
-                        <button type="button" id="reallike" class="reallike" onclick="likeButton(${comment_id})"><i class="fa-solid fa-heart fa-1x"></i></button>
+                        <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="heart" onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
                         </div>
                     </div>
                 </div>`
             $('#comments-box').prepend(temp_html)
         }
-    }
+    }}
     )
 }
 /// 게시물 수정
@@ -254,7 +291,7 @@ async function likeButton(comment_id) {
     }
     if (!localStorage.getItem("access")) {
         alert('로그인해주세요!')
-    }
+    } 
     const response = await fetch(`${backend_base_url}/article/${comment_id}/comment/like/`, {
         method: 'POST',
         headers: {
@@ -265,5 +302,19 @@ async function likeButton(comment_id) {
     }
     )
     response_json = await response.json()
+    console.log(response_json)
     window.location.reload()
 }
+
+// const icoHearts = document.querySelectorAll('.heart')
+// icoHearts.forEach(like => { 
+//     heart.addEventListener('click', () => {
+//         like.classList.toggle('active')
+
+//         if (heart.classList.contains('active')) {
+//             heart.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>`
+//         } else {
+//             heart.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>`
+//         }
+//     })
+// })
