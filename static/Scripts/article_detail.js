2c75ef386b6944dd69ec6f92f72f5f9bb1b78bf9
Moonmoo <ansaudwn1234@gmail.com>
2022-08-03 02:28:26 +0900

    폰트사이즈수정

--- static/Scripts/article_detail.js
+++ static/Scripts/article_detail.js
@@ -60,7 +60,7 @@ window.onload = async function articleDetail() {
             </div>
         </div>
         <div class="contentdiv">
-            <h3 class="content">${contents}</h3>
+            <h5 class="content">${contents}</h5>
         </div>
         <div>
         <div class="botediv">
@@ -84,7 +84,7 @@ window.onload = async function articleDetail() {
             </div>
         </div>
         <div class="contentdiv">
-            <h3 class="content">${contents}</h3>
+            <h5 class="content">${contents}</h5>
         </div>
         <div>
         <div class="botediv">
@@ -105,7 +105,7 @@ window.onload = async function articleDetail() {
         </div>
         <div class="contentdiv">
             <img src="http://127.0.0.1:8000${image}" alt="" />
-            <h3 class="content">${contents}</h3>
+            <h5 class="content">${contents}</h5>
         </div>
         <div>
         <div class="botediv">
@@ -130,7 +130,7 @@ window.onload = async function articleDetail() {
         </div>
         <div class="contentdiv">
             <img src="http://127.0.0.1:8000${image}" alt="" />
-            <h3 class="content">${contents}</h3>
+            <h5 class="content">${contents}</h5>
         </div>
         <div>
         <div class="botediv">
@@ -159,7 +159,7 @@ window.onload = async function articleDetail() {
                         <div><p class="cotime">${comment_created_at}</p></div>
                     </div>
                     <div class="commentdetail">
-                        <div class="comment"><h4>${comments}</h4></div>
+                        <div class="comment"><h6>${comments}</h6></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
                         <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="heart" onclick="likeButton(${comment_id})"height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
@@ -197,7 +197,7 @@ window.onload = async function articleDetail() {
                         </div>
                     </div>
                     <div class="commentdetail">
-                        <div class="comment"><h4>${comments}</h4></div>
+                        <div class="comment"><h6>${comments}</h6></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
                         <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="heart" onclick="likeButton(${comment_id})"height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
@@ -235,7 +235,7 @@ window.onload = async function articleDetail() {
                         </div>
                     </div>
                     <div class="commentdetail">
-                        <div class="comment"><h4>${comments}</h4></div>
+                        <div class="comment"><h6>${comments}</h6></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
                         <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="hearts" onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
@@ -252,7 +252,7 @@ window.onload = async function articleDetail() {
                         <div><p class="cotime">${comment_created_at}</p></div>
                     </div>
                     <div class="commentdetail">
-                        <div class="comment"><h4>${comments}</h4></div>
+                        <div class="comment"><h6>${comments}</h6></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
                         <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="hearts" onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
