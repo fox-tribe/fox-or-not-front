a9187197686831784cdcdce932d8fadfd5978880
Moonmoo <ansaudwn1234@gmail.com>
2022-08-03 00:14:03 +0900

    게시글 및 댓글 작성한 본인아닐시 수정삭제버튼없애기

--- static/Scripts/article_detail.js
+++ static/Scripts/article_detail.js
@@ -1,5 +1,9 @@
 const url = window.location.search.split('=')
 const obj_id = url[1]
+const payload = localStorage.getItem("payload")
+const words = payload.split(':');
+id = words[5].split(',')
+console.log(id[0])
 // 게시물 상세 페이지 부르기
 window.onload = async function articleDetail() {
     let articleDetail = async () => {
@@ -7,10 +11,13 @@ window.onload = async function articleDetail() {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem("access"),
             },
         })
+        
         // 로그인 로그아웃 회원가입 버튼 숨기기
         if (!localStorage.getItem("access")) {
+            
             let my_buttons = document.getElementById("my-buttons")
             my_buttons.style.visibility = "hidden"
         }
@@ -26,11 +33,15 @@ window.onload = async function articleDetail() {
         }
         response_json = await response.json()
         return response_json.articles
+
+        
     }
     // 게시물 상세 내용
     articleDetail().then((data) => {
         detail = response_json
         console.log(detail)
+        let login_id = id[0]
+        let article_author = detail['article_author']
         let nickname = detail['nickname']
         let image = detail['article_image']
         let title = detail['article_title']
@@ -39,7 +50,7 @@ window.onload = async function articleDetail() {
         let count_fox = detail['vote']['fox']
         let count_green = detail['vote']['green']
         let count_miss = detail['vote']['miss']
-        if (image == null) {
+        if (image == null && login_id != article_author) {
             let temp_html =
                 `<div class="titlediv">
                 <div class="writeinfo">
@@ -59,7 +70,31 @@ window.onload = async function articleDetail() {
         </div>
         </div>`
             $('#article-detail-box').prepend(temp_html)
-        } else {
+        } else if (image == null && login_id == article_author){
+            let temp_html =
+                `<div class="articlebuttons" id="article-buttons">
+                    <p class="comment-modify" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">게시글 수정</p>
+                    <p class="comment-delete" onclick="removeArticle()">삭제</p>
+                </div>
+                <div class="titlediv">
+                <div class="writeinfo">
+                <div><a>${nickname} - ${date}</a></div>
+            </div>
+            <div class="title"><h2>${title}</h2></div>
+            </div>
+        </div>
+        <div class="contentdiv">
+            <h3 class="content">${contents}</h3>
+        </div>
+        <div>
+        <div class="botediv">
+            <div class="boteb"><button type="button" class="bote" onclick="vote1()">🦊</button><p class="btext1">폭스입니다 (${count_fox})</p></div>
+            <div class="boteb"><button type="button" class="bote" onclick="vote2()">💚</button><p class="btext2">그린라이트 (${count_green})</p></div>
+            <div class="boteb"><button type="button" class="bote" onclick="vote3()">💔</button><p class="btext3">오해입니다 (${count_miss})</p></div>
+        </div>
+        </div>`
+            $('#article-detail-box').prepend(temp_html)
+        } else if (image != null && login_id != article_author){
             let temp_html =
                 `<div class="titlediv">
                 <div class="writeinfo">
@@ -80,14 +115,58 @@ window.onload = async function articleDetail() {
         </div>
         </div>`
             $('#article-detail-box').prepend(temp_html)
+        } else if (image != null && login_id == article_author){
+            let temp_html =
+                `<div class="articlebuttons" id="article-buttons">
+                    <p class="comment-modify" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">게시글 수정</p>
+                    <p class="comment-delete" onclick="removeArticle()">삭제</p>
+                </div>
+            <div class="titlediv">
+                <div class="writeinfo">
+                <div><a>${nickname} - ${date}</a></div>
+            </div>
+            <div class="title"><h2>${title}</h2></div>
+            </div>
+        </div>
+        <div class="contentdiv">
+            <img src="http://127.0.0.1:8000${image}" alt="" />
+            <h3 class="content">${contents}</h3>
+        </div>
+        <div>
+        <div class="botediv">
+            <div class="boteb"><button type="button" class="bote" onclick="vote1()">🦊</button><p class="btext1">폭스입니다 (${count_fox})</p></div>
+            <div class="boteb"><button type="button" class="bote" onclick="vote2()">💚</button><p class="btext2">그린라이트 (${count_green})</p></div>
+            <div class="boteb"><button type="button" class="bote" onclick="vote3()">💔</button><p class="btext3">오해입니다 (${count_miss})</p></div>
+        </div>
+        </div>`
+            $('#article-detail-box').prepend(temp_html)
         }
         for (let i = 0; i < detail['comment_set'].length; i++) {
+            let login_id = id[0]
+            let comment_author = detail['comment_set'][i]['comment_author']
+
             let comments = detail['comment_set'][i]['comment_contents']
             let comment_id = detail['comment_set'][i]['id']
             let nickname = detail['comment_set'][i]['nickname']
             let comment_created_at = detail['comment_set'][i]['comment_created_at']
             let comment_like_count = detail['comment_set'][i]['count']
-            if (comment_like_count == 0) {let temp_html =
+            if (comment_like_count == 0 && login_id != comment_author) {let temp_html =
+                `<div class="comments">
+                    <div class="cowriteinfo">
+                        <div><p class="cowriter">${nickname}</p></div>
+                        <div><p class="cotime">${comment_created_at}</p></div>
+                    </div>
+                    <div class="commentdetail">
+                        <div class="comment"><h4>${comments}</h4></div>
+                        <div style="display:flex; flex-direction:row; align-items:center">
+                        <svg xmlns="http://www.w3.org/2000/svg" id="heart-button" class="heart " onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                        <h5 style="margin:0; display:block">${comment_like_count}</h5>
+                        </div>
+                    </div>
+                </div>`
+            $('#comments-box').prepend(temp_html)
+        } else if (comment_like_count == 0 && login_id == comment_author){
+            let temp_html =
                 `<div class="comments">
                     <div class="cowriteinfo">
                         <div><p class="cowriter">${nickname}</p></div>
@@ -118,13 +197,13 @@ window.onload = async function articleDetail() {
                     <div class="commentdetail">
                         <div class="comment"><h4>${comments}</h4></div>
                         <div style="display:flex; flex-direction:row; align-items:center">
-                        <svg xmlns="http://www.w3.org/2000/svg" id="heart-button" class="heart " onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="heart" onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         <h5 style="margin:0; display:block">${comment_like_count}</h5>
                         </div>
                     </div>
                 </div>`
             $('#comments-box').prepend(temp_html)
-        } else {
+        } else if (comment_like_count != 0 && login_id == comment_author){
             let temp_html =
                 `<div class="comments">
                     <div class="cowriteinfo">
@@ -152,6 +231,22 @@ window.onload = async function articleDetail() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="commentdetail">
+                        <div class="comment"><h4>${comments}</h4></div>
+                        <div style="display:flex; flex-direction:row; align-items:center">
+                        <svg xmlns="http://www.w3.org/2000/svg" id="hearts-button" class="heart" onclick="likeButton(${comment_id})" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
+                        <h5 style="margin:0; display:block">${comment_like_count}</h5>
+                        </div>
+                    </div>
+                </div>`
+            $('#comments-box').prepend(temp_html)
+        } else if (comment_like_count != 0 && login_id != comment_author){
+            let temp_html =
+                `<div class="comments">
+                    <div class="cowriteinfo">
+                        <div><p class="cowriter">${nickname}</p></div>
+                        <div><p class="cotime">${comment_created_at}</p></div>
                     </div>
                     <div class="commentdetail">
                         <div class="comment"><h4>${comments}</h4></div>
