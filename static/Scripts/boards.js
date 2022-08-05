aa62b0ce9b7acae2f9ed1def49a29fd308c8fd13
attabooi <joonqpa@gmail.com>
2022-08-05 19:39:58 +0900

    투표순 게시르 리스팅 픽스

--- static/Scripts/boards.js
+++ static/Scripts/boards.js
@@ -68,6 +68,8 @@ async function search() {
     response_json = await response.json()
     return response_json.articles
 }
+
+// 페이지네이션
 const PagingConf = {
     totalCount: 100,
     numbersPerPage: 5,
@@ -165,9 +167,7 @@ function postClick() {
 
 function getPosts() {
     $('#list-post').empty()
-
     const pageInfo = getPageInfo();
-
     let page = pageInfo.currentPage;
     let offset = pageInfo.numbersPerPage;
     let params = '?page=' + page;
@@ -178,6 +178,7 @@ function getPosts() {
         data: {},
         async: false,
         success: function (response) {
+            try{
             let posts = response['results'];
             if (posts[0]['board'] == '10대') {
                 let board_html = `<b id="board-name">10대 게시판</b>`
@@ -203,15 +204,44 @@ function getPosts() {
                 let board_html = `<b id="board-name">자유 게시판</b>`
                 $('#board-name').empty()
                 $('#board-name').append(board_html)
-            } 
-            if (decoded_name == 'HOT'){
+            } else if (decoded_name == 'HOT'){
                 let board_html = `<b id="board-name">HOT 게시판</b>`
                 $('#board-name').empty()
                 $('#board-name').append(board_html)
+                // HOT 게시판 리스팅
+                $.ajax({
+                    type: 'GET',
+                    url: `${backend_base_url}/article/voteCount`,
+                    data: {},
+                    async: false,
+                    success: function (response) {
+                        response.reverse()
+                        if (response['results'] != 0) {
+                            for (let i = 0; i < response.length; i++) {
+                                let cnt = i + 1
+                                const id = response[i]['id']
+                                const title = response[i]['article_title']
+                                const author = response[i]['author']
+                                const date = response[i]['article_post_date']
+                                const vote = response[i]['vote']['count']
+                                let temp_html = `<div class="post-line row" onclick="location.href='${frontend_base_url}/detail.html?id=${id}'">
+                                                    <div class="post-number col-1" scope="col">${cnt}.</div>
+                                                    <div class="post-title col-6" scope="col"><b>${title}</b></div>
+                                                    <div class="post-author col col-md-col1" scope="col">${author}</div>
+                                                    <div class="post-date col" scope="col">${date}</div>
+                                                    <div class="post-date col" scope="col">투표:${vote}</div>
+                                                </div>`
+                                $('#list-post').append(temp_html)
+                    }
+                }
+            }
+            }
+            )
             }
             
             if (response['results'] != 0) {
                 let posts = response['results'];
+                console.log(response)
                 console.log(posts)
                 for (let i = 0; i < posts.length; i++) {
                     let cnt = i + 1
@@ -228,11 +258,44 @@ function getPosts() {
                     $('#list-post').append(temp_html)
                 }
             }
-        },
+        } catch (err) {
+            let board_html = `<b id="board-name">HOT 게시판</b>`
+                $('#board-name').empty()
+                $('#board-name').append(board_html)
+                // HOT 게시판 리스팅
+                $.ajax({
+                    type: 'GET',
+                    url: `${backend_base_url}/article/voteCount/board/`,
+                    data: {},
+                    async: false,
+                    success: function (response) {
+                        response.reverse()
+                        if (response['results'] != 0) {
+                            for (let i = 0; i < response.length; i++) {
+                                let cnt = i + 1
+                                const id = response[i]['id']
+                                const title = response[i]['article_title']
+                                const author = response[i]['author']
+                                const date = response[i]['article_post_date']
+                                const vote = response[i]['vote']['count']
+                                if (vote == 0 ){
+
+                                } else {
+                                let temp_html = `<div class="post-line row" onclick="location.href='${frontend_base_url}/detail.html?id=${id}'">
+                                                    <div class="post-number col-1" scope="col">${cnt}.</div>
+                                                    <div class="post-title col-6" scope="col"><b>${title}</b></div>
+                                                    <div class="post-author col col-md-col1" scope="col">${author}</div>
+                                                    <div class="post-date col" scope="col">${date}</div>
+                                                    <div class="post-date col" scope="col">투표:${vote}</div>
+                                                </div>`
+                                $('#list-post').append(temp_html)
+                                }
+                    }
+
+        }}})}},
         fail: function (response) {
             console.log("fail")
             // window.location.reload()
         }
     });
 }
-
