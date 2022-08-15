bde2915dd1df1a0a27a3c4ea0c70a53b26d9babc
seonyoungBaek <rk4418308@gmail.com>
2022-08-15 20:45:13 +0900

    api url 수정

--- static/JS/api.js
+++ static/JS/api.js
@@ -1,7 +1,5 @@
-const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5500"
-// const backend_base_url = "https://api.fox-or-not.com"
-// const frontend_base_url = "https://fox-or-not.com"
+const backend_base_url = "https://api.fox-or-not.com"
+const frontend_base_url = "https://fox-or-not.com"
 
 
 
